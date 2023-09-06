d4d58f56d2b544ab1b9855d2b950d909924b3f63
sabeelmhd <sabeelzaim@gmail.com>
2023-09-07 01:42:27 +0530

    User interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,31 @@
-
-import './App.css';
+// App.js
+import React, { useState } from 'react'
+import CodeEditor from '@monaco-editor/react'
+import './App.css'
 
 function App() {
+  const [code, setCode] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <form>
-          <label>Enter the code</label>
-          <input type='text'></input>
-          <input type='submit'></input>
-        </form>
-        
+    <div className='App'>
+      <header className='App-header'>
+        <div className='container'>
+          <h1 className='title'>Code Analyzer</h1>
+          <p className='subtitle'>Enter your code below and click "Analyze"</p>
+          <div className='code-editor'>
+            <CodeEditor
+              language='java'
+              value={code}
+              onChange={(value) => setCode(value)}
+              height='500px'
+              width='800px'
+            />
+          </div>
+          <button className='analyze-button'>Analyze</button>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
