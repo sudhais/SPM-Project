963177fa3dce21c2e68ad6dbc1c46992a1a2ed55
Sudhais <87361938+sudhais@users.noreply.github.com>
2023-10-14 00:22:18 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 // App.js
 import React, { useState } from 'react'
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import './App.css'
 
 //component
 import Home from './components/Home'
 import History from './pages/History'
+
+import HomePage from "./pages/HomePage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   const [code, setCode] = useState('')
@@ -14,12 +18,29 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' Component={Home}></Route>
+        <Route path='/' 
+          element={
+            <ProtectedRoutes>
+              <HomePage/>
+            </ProtectedRoutes> 
+          }
+          />
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/chech' Component={Home}></Route>
         <Route path='/test' element={<History/>} ></Route>
       </Routes>
     </BrowserRouter>
     
   )
+}
+
+export function ProtectedRoutes(props){
+  if(localStorage.getItem('user')){
+    return props.children
+  }else{
+    return <Navigate to="/login"/>
+  }
 }
 
 export default App
